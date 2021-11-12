30cbd539b509375b27f34341613ec8bf32da737b
Tomas Fridekrans <vindicated@hotmail.com>
2021-11-12 08:47:04 +0100

    added structure and testing a get function

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ const SCENES = [
     title: "entry",
     goTos: ["computer", "door", "keypad"],
   },
+  {
+    title: "light switch",
+    goTos: ["switch board"],
+  },
 ]
 const INVENTORY = []
+
+function getGoTos() {
+  console.log(SCENES[currentRoom].goTos)
+}
+
+getGoTos()
